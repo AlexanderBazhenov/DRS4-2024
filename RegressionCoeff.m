cf99092f6deaec90f496c17e96a1e73329f5e7ad
AlexanderBazhenov <59746051+AlexanderBazhenov@users.noreply.github.com>
2024-09-23 12:17:49 +0300

    Add files via upload

---
+++ RegressionCoeff.m
@@ -0,0 +1,54 @@
+% 2024-09-20
+function [b_int, b_out] = RegressionCoeff(X, fnX, ch, bin)
+%
+%tic
+% 2024-09-17
+% 2024-09-29 DATA INT OUT
+[yarrayint, yarrayout] = DataIntOut(X, fnX, ch, bin);
+##[yarrayint] = DRSCalibrationDataInt (X, fnX, ch, bin);
+##[yarrayout] = DRSCalibrationDataExt (X, fnX, ch, bin);
+%toc - 3.7s
+% sort data
+[Xs, inds] = sort(X);
+Ysint = yarrayint(inds);
+Ysout = yarrayout(inds);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    REGRESSION   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2024-08-27
+% 2024-09-17
+Xi = [ Xs'.^0 Xs'];
+%
+y = mid(Ysout);
+epsilon = rad(Ysout);
+% https://github.com/szhilin/octave-interval-examples/blob/master/SteamGenerator.ipynb
+irp_DRSout = ir_problem(Xi, y', epsilon');
+%
+y = mid(Ysint);
+epsilon = rad(Ysint);
+irp_DRSint = ir_problem(Xi, y', epsilon');
+%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+b_out = ir_outer(irp_DRSout);
+Ys = Ysint
+y = mid(Ys)/16384-0.5;
+epsilon = rad(Ys)/16384;
+[tolmax,argmax, env] = tolsolvty(Xi,Xi,y'-epsilon',y'+epsilon',1)
+if tolmax > 0
+  b_int = ir_outer(irp_DRSint);
+  display('tolmax > 0')
+else
+  display('tolmax < 0')
+  [envnegind, envneg] = find(env(:,2) < 0)
+  indtoout = env(envnegind,1)
+##  y(indtoout) = mid(Ysout(indtoout))/16384-0.5;
+##  epsilon(indtoout) = rad(Ysout(indtoout))/16384;
+  y(indtoout) = mid(Ysout(indtoout));
+  epsilon(indtoout) = rad(Ysout(indtoout));
+  [tolmax,argmax, env] = tolsolvty(Xi,Xi,y'-epsilon',y'+epsilon',1)
+  irp_DRSint = ir_problem(Xi, y', epsilon');
+  b_int = ir_outer(irp_DRSint);
+endif
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
