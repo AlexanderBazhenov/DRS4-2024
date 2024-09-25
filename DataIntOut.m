3481a2fdbd9bfaaf8b7ca68988c8f726e17c2d64
AlexanderBazhenov <59746051+AlexanderBazhenov@users.noreply.github.com>
2024-09-25 15:14:48 +0300

    Add files via upload

--- DataIntOut.m
+++ DataIntOut.m
@@ -2,5 +2,6 @@
 function [yarrayint, yarrayout] = DataIntOut(X, fnX, ch, bin)
 % 2024-09-17
 [yarrayint] = DRSCalibrationDataInt (X, fnX, ch, bin);
-[yarrayout] = DRSCalibrationDataExt (X, fnX, ch, bin);
+%[yarrayout] = DRSCalibrationDataExt (X, fnX, ch, bin);
+[yarrayout] = DRSCalibrationDataExt2 (X, fnX, ch, bin);
 end
